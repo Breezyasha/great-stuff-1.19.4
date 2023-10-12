41aab8bdd388a7b210f26ab29a5a412959147579
Breezyasha <23orignald23@gmail.com>
2023-10-12 16:13:47 -0400

    More Item

--- src/main/java/com/greatstuff/GreatStuffMod.java
+++ src/main/java/com/greatstuff/GreatStuffMod.java
@@ -1,5 +1,6 @@
 package com.greatstuff;
 
+import com.greatstuff.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -12,6 +13,6 @@ public class GreatStuffMod implements ModInitializer {
     @Override
     public void onInitialize() {
 
-LOGGER.info("Hello Fabric World!");
+        ModItems.registerModItems();
     }
 }
--- src/main/java/com/greatstuff/item/ModItems.java
+++ src/main/java/com/greatstuff/item/ModItems.java
@@ -1,10 +1,20 @@
 package com.greatstuff.item;
 
 import com.greatstuff.GreatStuffMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 public class ModItems {
+    public static final Item RAW_TAN = registerItem("raw_tan" , new Item(new FabricItemSettings()));
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, new Identifier(GreatStuffMod.MOD_ID, name), item);
+    }
     public static void registerModItems(){
-        GreatStuffMod.LOGGER.debug("Registering items for" + GreatStuffMod.MOD_ID);
+        GreatStuffMod.LOGGER.info("Registering items for" + GreatStuffMod.MOD_ID);
 
     }
 }
