d2ba98eca4d4864a835b94f5b7efb735bfb899fa
Breezyasha <23orignald23@gmail.com>
2023-10-12 15:55:45 -0400

    Item

---
+++ src/main/java/com/greatstuff/item/ModItems.java
@@ -0,0 +1,11 @@
+package com.greatstuff.item;
+
+import com.greatstuff.GreatStuffMod;
+
+public class ModItems {
+    public static void registerModItems(){
+        GreatStuffMod.LOGGER.debug("Registering items for" + GreatStuffMod.MOD_ID);
+
+    }
+}
+
